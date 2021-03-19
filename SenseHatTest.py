bd8d111e0d3e2357137a320a625d67c5a249e7be
Howard Huckle <howardhuckle@yahoo.co.uk>
2021-03-19 19:35:53 +0000

    SenseHat matrix tests

--- SenseHatTest.py
+++ SenseHatTest.py
@@ -1,11 +1,36 @@
-from gpiozero import LED
+from sense_hat import SenseHat
 from time import sleep
 
-led = LED(25)
+sense=SenseHat()
 
-while True:
-    led.on()
+sense.clear()
+
+yellow=(255,255,0)
+blue=(0,0,255)
+
+sense.show_message("Hello World",text_colour=yellow, back_colour=blue,scroll_speed=0.05)
+
+g=(0,255,0)
+b=(0,0,255)
+
+creeper=[
+    g,g,g,g,g,g,g,g,
+    g,g,g,g,g,g,g,g,
+    g,g,g,g,g,b,b,g,
+    g,g,g,g,g,b,b,g,
+    g,g,g,b,b,g,g,g,
+    g,g,b,b,b,b,g,g,
+    g,g,b,b,b,b,g,g,
+    g,g,b,g,g,b,g,g,   
+    ]
+
+sense.set_pixels(creeper)
+
+number=0
+while number<6:
     sleep(1)
-    led.off()
-    sleep(1)
-    
+    sense.flip_h()
+    number+=1
+
+sense.clear()
+
